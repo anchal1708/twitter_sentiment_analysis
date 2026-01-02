2d1a504f9bad6d17b81cc42a22fcbabf6cea14ba
anchal1708 <163990199+anchal1708@users.noreply.github.com>
2026-01-02 10:31:26 +0530

    Update sentiment prediction response format
    Refactor sentiment prediction to return integer value.

--- api.py
+++ api.py
@@ -2,14 +2,17 @@ import pickle
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pathlib import Path
-import pickle
+
+app = FastAPI()
+
 
 BASE_DIR = Path(__file__).resolve().parent
-MODEL_PATH = BASE_DIR / "src" / "sentiment_model.pkl"
-VECTORIZER_PATH = BASE_DIR / "src" / "vectorizer.pkl"
+MODEL_PATH = BASE_DIR / "sentiment_model.pkl"
+VECTORIZER_PATH = BASE_DIR / "vectorizer.pkl"
 
 with open(MODEL_PATH, "rb") as f:
     model = pickle.load(f)
+
 
 with open(VECTORIZER_PATH, "rb") as f:
     vectorizer = pickle.load(f)
@@ -18,16 +21,15 @@ with open(VECTORIZER_PATH, "rb") as f:
 class TweetRequest(BaseModel):
     text: str
 
+
 @app.get("/")
 def home():
     return {"message": "Twitter Sentiment Analysis API is running"}
+
 
 @app.post("/predict")
 def predict_sentiment(request: TweetRequest):
     text_vector = vectorizer.transform([request.text])
     prediction = model.predict(text_vector)[0]
-
-    sentiment = "Positive" if prediction == 1 else "Negative"
-    return {"sentiment": sentiment}
-
+    return {"sentiment": int(prediction)}
 
